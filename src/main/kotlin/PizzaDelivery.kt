ad626cc5501f90ea3cc64b279145e97d1317d692
Matheus Gambati <matheusgambati@gmail.com>
2018-03-06 00:27:07 -0400

    Setup inicial

---
+++ src/main/kotlin/PizzaDelivery.kt
@@ -0,0 +1,10 @@
+import util.HibernateUtil
+
+class PizzaDelivery {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            HibernateUtil
+            println("Oi!")
+        }
+    }
+}
