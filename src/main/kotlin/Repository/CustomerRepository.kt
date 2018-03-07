948489d0fbb9df486c81d0d789cd4957f3b81e28
Matheus Gambati <matheusgambati@gmail.com>
2018-03-06 20:05:40 -0400

    remove hibernate files

--- src/main/kotlin/Repository/CustomerRepository.kt
+++
@@ -1,7 +0,0 @@
-package Repository
-
-import Entity.Customer
-
-class CustomerRepository : CrudRepository<Customer, Int>() {
-    override fun getDomainClass(): Class<Customer> = Customer::class.java
-}
